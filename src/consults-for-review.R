291768016a2ea096740590eb7f6c54785a7d9b53
Corey Handelsman <chandelsman@gmail.com>
2021-04-08 13:18:10 -0600

    Updated to run 2020q1 reports

--- src/consults-for-review.R
+++ src/consults-for-review.R
@@ -99,11 +99,11 @@ consult_review <-
   consult_ext %>% 
   bind_rows(consult_int) %>% 
   bind_rows(consult_iop) %>% 
-  filter(as_date(`Collected date`) >= "2020-10-01" & 
-           as_date(`Collected date`) <= "2020-12-31", 
+  filter(as_date(`Collected date`) >= "2021-01-01" & 
+           as_date(`Collected date`) <= "2021-03-31", 
          Correlation != "YES" & 
            Correlation != "Select") %>% 
   arrange(Client, `Primary Pathologist`)
 
 write_csv(consult_review, 
-          here("output", "2020q4-consults-for-review.csv"))
+          here("output", "2021q1-consults-for-review.csv"))
